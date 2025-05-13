1cbe4dfb3756e00cad44653be1132571b17eb96b
Hayat A <xaetbek@gmail.com>
2025-05-13 15:10:41 -0700

    create an ArrayList of Asset objects.
    Load it with your Assets. Include at least 2 houses (you have a vacation home!) and at least two vehicles. Use descriptions like "my house" or "Tom's truck" for the assets.
    
    Now, loop thru the Asset collection displaying the description of each asset, the date you acquired it, how much you paid for it, and its value.
    
    When that works, go back and include in the display either the address of the house or the year and make/model of the vehicle. You will need to use instanceof when you loop through the assets to detect the type of asset it is. Once you know it is a House or Vehicle, you will need to downcast it so that you can call the methods of the specific type.

--- AssetManager/src/main/java/org/pluralsight/Main.java
+++ AssetManager/src/main/java/org/pluralsight/Main.java
@@ -1,9 +1,35 @@
 package org.pluralsight;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Asset> assets = new ArrayList<>();
 
+        House myHouse = new House("my house", "01/01/2001", 8000000.00, "1310 Turk St.", 1, 100, 200);
+        assets.add(myHouse);
+
+        House rentalHouse = new House("airbnb house", "12/31/2003", 3000000.00, "1227 Northpoint Dr.", 1, 140, 244);
+        assets.add(rentalHouse);
+
+        Vehicle myVehicle = new Vehicle("my car", "04/30/2024", 31000.00, "Lincoln Navigator", 2020, 20300);
+        assets.add(myVehicle);
+
+        Vehicle rentalVehicle = new Vehicle("rental car", "04/30/2025", 131000.00, "Cadillac Escalade", 2025, 300);
+        assets.add(rentalVehicle);
+
+        for (Asset asset : assets) {
+            if (asset instanceof House) {
+                System.out.println("\nHouse Asset: ");
+                System.out.println("This is " + asset.getDescription() + ". I bought it in " + asset.getDateAcquired() +
+                        " for the price of " + asset.getValue() + ". The original cost is: " + asset.getOriginalCost());
+            }
+            if(asset instanceof Vehicle){
+                System.out.println("\nVehicle Asset");
+                System.out.println("This is " + asset.getDescription() + ". I acquired it in " + asset.getDateAcquired() + " for the price of " + asset.getOriginalCost() + " \nThe year is " + ((Vehicle) asset).getYear() + " and the manufacturer is " + ((Vehicle) asset).getMakeModel());
+            }
+        }
     }
 }
